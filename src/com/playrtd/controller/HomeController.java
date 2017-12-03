856b251f129d4f830f65dc7a923301d47c841f47
sbernardy <samuel.bernardy@gmail.com>
2017-12-02 23:17:26 -0500

    INital commit with project directory.

---
+++ src/com/playrtd/controller/HomeController.java
@@ -0,0 +1,35 @@
+package com.playrtd.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+/*
+ * author: samuelbernardy
+ * author: yasrodriguez
+ * author: josephdgarza
+ */
+
+@Controller
+public class HomeController {
+
+	@RequestMapping("/")
+	public String main() {
+		return "";
+	}
+
+	@RequestMapping("/return")
+	public String loggedIn() {
+		return "return";
+	}
+
+	@RequestMapping("/seerecent")
+	public String seeRecent() {
+		return "seerecent";
+	}
+
+	@RequestMapping("/rolled")
+	public String rolled() {
+		return "gameon";
+	}
+}
