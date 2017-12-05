dd76ffdec83036ef13273df147e31da857efe133
Yasmin Rodriguez <yasmin.rodriguez@gmail.com>
2017-12-05 16:30:18 -0500

    Added the ability to search by twitch game name or game ID

--- WebContent/WEB-INF/views/twitch.jsp
+++ WebContent/WEB-INF/views/twitch.jsp
@@ -3,30 +3,46 @@
 	<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c"%>
 <!DOCTYPE html>
 <html>
-<head>
-<title>Twitch Live Stream</title>
-</head>
   <body>
+  <form action = "getgamename">
+  Enter Twitch Game ID:
+  <input type = "text" name="twitchgameid" value="32399">
+  <input type = "submit" value="Submit">
+  </form>
+   <p> ${gamename}</p>
+ 
+ <form action = "getgameid">
+  Enter Twitch Game Name:
+  <input type = "text" name="twitchgamename" value="Counter-Strike: Global Offensive">
+  <input type = "submit" value="Submit">
+  </form>
+ <p> ${gameid}</p>
+    
+  <a href="getstream">Sample Stream</a><br>
+    
     <!-- Add a placeholder for the Twitch embed -->
     <div id="twitch-embed"></div>
 
     <!-- Load the Twitch embed script -->
     <script src="https://embed.twitch.tv/embed/v1.js"></script>
 
-    <!-- Create a Twitch.Embed object that will render within the "twitch-embed" root element. -->
+    <!--
+      Create a Twitch.Embed object that will render
+      within the "twitch-embed" root element.
+    -->
     <script type="text/javascript">
-    var embed = new Twitch.Embed("twitch-embed", {
+      var embed = new Twitch.Embed("twitch-embed", {
         width: 854,
         height: 480,
         channel: "${channel}",
         layout: "video",
         autoplay: false
       });
-      
+
       embed.addEventListener(Twitch.Embed.VIDEO_READY, () => {
-          var player = embed.getPlayer();
-          player.play();
-        });
+        var player = embed.getPlayer();
+        player.play();
+      });
     </script>
-  </body>
+    </body>
 </html>
--- src/com/playrtd/controller/TwitchController.java
+++ src/com/playrtd/controller/TwitchController.java
@@ -1,6 +1,8 @@
 package com.playrtd.controller;
 
 import java.io.IOException;
+import java.net.URLEncoder;
+
 import org.apache.http.HttpHost;
 import org.apache.http.HttpResponse;
 import org.apache.http.client.ClientProtocolException;
@@ -13,18 +15,27 @@ import org.json.JSONObject;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
 import com.playrtd.util.Credentials;
 
 @Controller
 public class TwitchController {
 
 	@RequestMapping("/twitch")
-	public String getTwitchStreamByGameID(Model model) {
+	public String getTwitchPage() {
+		return "twitch";
+	}
 
-		// This should be the Twitch game ID for the game recommended
+	@RequestMapping("/getstream")
+	public String getTwitchStream(Model model) {
+
+		// Twitch game ID for the game recommended
 		String gameId = "32399"; // Counter-Strike
 		String channelName = "";
 
+		// Gets all live streams for the game ID, then selects the first stream in the
+		// array (most views)
 		try {
 			HttpClient http = HttpClientBuilder.create().build();
 			HttpHost host = new HttpHost("api.twitch.tv", 443, "https");
@@ -36,11 +47,16 @@ public class TwitchController {
 			JSONObject json = new JSONObject(jsonString);
 			JSONArray allStreams = json.getJSONArray("data");
 			JSONObject firstStream = allStreams.getJSONObject(0);
-			
+
 			String thumbnailUrl = firstStream.getString("thumbnail_url");
-			System.out.println("Thumbnail: " + thumbnailUrl);
-			//TODO Parse channel name from thumbnail URL
+			System.out.println(thumbnailUrl);
+			
+			// TODO Parse channel name from thumbnail URL
 			String pattern = "_user_channelNameHere-";
+
+			// TODO Twitch channel to play
+			channelName = "monstercat";
+			model.addAttribute("channel", channelName);
 
 		} catch (ClientProtocolException e) {
 
@@ -50,11 +66,67 @@ public class TwitchController {
 			e.printStackTrace();
 		}
 
-		// This variable should have the name of the Twitch channel that has the highest
-		// views for the video game recommended
-		channelName = "monstercat";
-		model.addAttribute("channel", channelName);
 		return "twitch";
 	}
 
+	@RequestMapping("/getgamename")
+	public String getTwitchGameName(Model model, @RequestParam("twitchgameid") String gameId) {
+		String gameName = "";
+
+		try {
+			HttpClient http = HttpClientBuilder.create().build();
+			HttpHost host = new HttpHost("api.twitch.tv", 443, "https");
+			HttpGet getPage = new HttpGet("/helix/games?id=" + gameId);
+			getPage.setHeader("Client-ID", Credentials.TWITCH_CLIENT_ID);
+			HttpResponse resp = http.execute(host, getPage);
+			String jsonString = EntityUtils.toString(resp.getEntity());
+
+			JSONObject json = new JSONObject(jsonString);
+			JSONArray dataArr = json.getJSONArray("data");
+			JSONObject game = dataArr.getJSONObject(0);
+			gameName = game.getString("name");
+
+			model.addAttribute("gamename", gameName);
+
+		} catch (ClientProtocolException e) {
+
+			e.printStackTrace();
+		} catch (IOException e) {
+
+			e.printStackTrace();
+		}
+		return "twitch";
+	}
+
+	@RequestMapping("/getgameid")
+	public String getTwitchGameId(Model model, @RequestParam("twitchgamename") String gameName) {
+		String gameId = "";
+
+		try {
+			HttpClient http = HttpClientBuilder.create().build();
+			HttpHost host = new HttpHost("api.twitch.tv", 443, "https");
+			
+			HttpGet getPage = new HttpGet("/helix/games?name=" + URLEncoder.encode(gameName, "UTF-8"));
+			getPage.setHeader("Client-ID", Credentials.TWITCH_CLIENT_ID);
+			HttpResponse resp = http.execute(host, getPage);
+			String jsonString = EntityUtils.toString(resp.getEntity());
+
+			JSONObject json = new JSONObject(jsonString);
+			JSONArray dataArr = json.getJSONArray("data");
+			// System.out.println(dataArr);
+			JSONObject game = dataArr.getJSONObject(0);
+			gameId = game.getString("id");
+			// System.out.println("Game " + gameName);
+
+			model.addAttribute("gameid", gameId);
+
+		} catch (ClientProtocolException e) {
+
+			e.printStackTrace();
+		} catch (IOException e) {
+
+			e.printStackTrace();
+		}
+		return "twitch";
+	}
 }
